eed1e98c8f503f31d0b34726d79b09c2e8b34559
백용훈 <125546062+dndall7352@users.noreply.github.com>
2023-11-21 10:23:17 +0900

    Delete smart_attendance directory

--- smart_attendance/src/main/java/egovframework/atoz/main/mapper/CompanyMapper.java
+++
@@ -1,7 +0,0 @@
-package egovframework.atoz.main.mapper;
-
-import egovframework.atoz.main.model.CompanyVo;
-
-public interface CompanyMapper {
-	CompanyVo companyInfo(int com_number) throws Exception;
-}
